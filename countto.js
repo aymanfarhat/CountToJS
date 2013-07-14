498dd79cb351eb98221a27e1fa77532b4bd06f86
aymanfarhat <ayman.farhat1@gmail.com>
2013-07-14 14:57:25 +0300

    Removed my old braces convention in JS

--- countto.js
+++ countto.js
@@ -28,38 +28,32 @@
  */
 
 (function($) {
-$.fn.countTo = function(options) 
-{
+$.fn.countTo = function(options){
     options = $.extend({}, $.fn.countTo.defaults, options || {});
 
     var loops = Math.ceil(options.speed / options.refreshInterval),
         increment = (options.to - options.from) / loops;
 
-    return $(this).each(function() 
-    {
+    return $(this).each(function(){
         var _this = this,
             loopCount = 0,
             value = options.from,
             interval = setInterval(updateTimer, options.refreshInterval);
 
-        function updateTimer() 
-        {
+        function updateTimer(){
             value += increment;
             loopCount++;
             $(_this).html(value.toFixed(options.decimals));
 
-            if (typeof(options.onUpdate) == 'function') 
-            {
+            if (typeof(options.onUpdate) == 'function'){
                 options.onUpdate.call(_this, value);
             }
 
-            if (loopCount >= loops) 
-            {
+            if (loopCount >= loops){
                 clearInterval(interval);
                 value = options.to;
 
-                if (typeof(options.onComplete) == 'function') 
-                {
+                if (typeof(options.onComplete) == 'function'){
                     options.onComplete.call(_this, value);
                 }
             }
